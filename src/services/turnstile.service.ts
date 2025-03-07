f862e715412ed4c9952276cd7a0f6f47d3970f66
Oskari Forsblom <oskari.forsblom@gmail.com>
2025-03-07 10:10:57 +0200

    error

--- src/services/turnstile.service.ts
+++ src/services/turnstile.service.ts
@@ -1,32 +1,44 @@
-import { HttpService, } from '@nestjs/axios';
-import { Inject, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import {
+  Inject,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { firstValueFrom, catchError } from 'rxjs';
 import { ITurnstileOptions } from '../interfaces/turnstile';
 
 @Injectable()
 export class TurnstileService {
-    private BASE_URL = 'https://challenges.cloudflare.com';
+  private BASE_URL = 'https://challenges.cloudflare.com';
 
-    constructor(private readonly httpService: HttpService, @Inject('TurnstileServiceOptions') private readonly options: ITurnstileOptions
-    ) { }
+  constructor(
+    private readonly httpService: HttpService,
+    @Inject('TurnstileServiceOptions')
+    private readonly options: ITurnstileOptions,
+  ) {}
 
-    async validateToken(token: string) {
-        console.log(token)
-        const { data } = await firstValueFrom(this.httpService.post(`${this.BASE_URL}/turnstile/v0/siteverify`, {
-            response: token,
-            secret: this.options.secretKey,
-            headers: {
-                'content-type': 'application/x-www-form-urlencoded',
-            },
-        }).pipe(catchError((error) => {
-            console.log(error)
-
+  async validateToken(token: string) {
+    const { data } = await firstValueFrom(
+      this.httpService
+        .post(`${this.BASE_URL}/turnstile/v0/siteverify`, {
+          response: token,
+          secret: this.options.secretKey,
+          headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+          },
+        })
+        .pipe(
+          catchError((error) => {
             if (this.options.onError) this.options.onError(error);
 
-            throw new InternalServerErrorException('Failed turnstile verification.')
-        })));
+            throw new InternalServerErrorException(
+              'Failed turnstile verification.',
+              error,
+            );
+          }),
+        ),
+    );
 
-        return data;
-    }
-
+    return data;
+  }
 }
